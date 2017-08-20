84aaa0a6d795faa6824d26d68ab35480629f0aad
Parse Pashtun <parse@pashtun.de>
2017-08-20 12:39:01 +0200

    added API file and try to display the output in HTML. it works in the console

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as readableAPI from './readableAPI'
 
 class App extends Component {
+  state = {categories: [],
+    tanzen: []
+  }
+
+//to get all Categories.
+getCategories = () => {
+    readableAPI.get().then(categories => {this.setState({categories}), 
+    console.log("AusApp heraus: ", categories),
+    console.log("State:", this.state.categories)
+    })
+  }
+
+// API Call to get all the Books initiall before the WebSite is rendered.
+  componentDidMount() {
+    this.getCategories()
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +28,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React Basser</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {(this.state.categories).map(category => (
+           <h1>{category.name}</h1>
+        ))}
       </div>
     );
   }
---
+++ src/readableAPI.js
@@ -0,0 +1,50 @@
+//Basics are cloned of the Udacity BooksAPI out of the MyRead Project.
+
+//API Url pointing to localhost or Heroku
+// const api = "http://localhost:5001"; 
+const api = "https://evening-harbor-34965.herokuapp.com";
+
+// Generate a unique token for storing data on the backend server.
+let token = localStorage.token
+if (!token)
+  token = localStorage.token = Math.random().toString(36).substr(-8)
+
+const headers = {
+  'Accept': 'application/json',
+  'Authorization': token
+}
+
+//export const get = () => console.log(`${api}/categories`,{headers})
+
+export const get = () =>
+  fetch(`${api}/categories`, {headers})
+    .then(res => res.json())
+
+/*
+export const getAll = () =>
+  fetch(`${api}/books`, { headers })
+    .then(res => res.json())
+    .then(data => data.books)
+
+export const update = (book, shelf) =>
+  fetch(`${api}/books/${book.id}`, {
+    method: 'PUT',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ shelf })
+  }).then(res => res.json())
+
+export const search = (query, maxResults) =>
+  fetch(`${api}/search`, {
+    method: 'POST',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ query, maxResults })
+  }).then(res => res.json())
+    .then(data => data.books)
+
+*/
