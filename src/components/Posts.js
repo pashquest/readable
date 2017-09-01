2f0c31c3bdcd7ef926d6b614090dc978c6d94884
Parse Pashtun <parse@pashtun.de>
2017-09-01 22:50:16 +0200

    Added moment and lodash library ** Added Post initial VoteScoe Sort and timestamp conversian

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,15 +2,27 @@ import React, {Component} from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import {getPosts} from '../actions/postsActions';
+import _orderBy from 'lodash.orderby' //used for sorting the posts
+import moment from 'moment' // used for the timestamp conversion
 
 
 class Posts extends Component {
+
 render() {
+
+    let sortedPosts = []
+    
+    //Sorting the Pots and save it in sortedPosts Array
+    if(typeof (this.props.posts.posts) != 'undefined'){
+        sortedPosts = _orderBy(this.props.posts.posts,["voteScore"],["desc"]);
+    }
         return (
            <div> 
-                {(this.props.posts.posts || []).map(post => 
+                {(sortedPosts || []).map(post =>{             
+                return(
                 <ol key = {post.id}>
-                    <h4 key={post.id}><strong>id:</strong> {post.timestamp} </h4>
+                    <h4 key={post.id}><strong>id:</strong> {post.id} </h4>
+                    <h4> <font color="red">timestamp:</font> {moment(post.timestamp).format('lll')}</h4>
                     <h4> <font color="red">Title:</font> {post.title}</h4>
                     <h4><font color="red">body:</font> {post.body}</h4>
                     <h4><font color="red">Author:</font> {post.author}</h4>
@@ -18,7 +30,8 @@ render() {
                     <h4><font color="red">VoteScore</font> {post.voteScore}</h4>
                     <h4><font color="red">Deleted:</font>{post.deleted} </h4>
                 </ol>
-                )} 
+                )}) 
+                }
             </div>             
         )}
 }
